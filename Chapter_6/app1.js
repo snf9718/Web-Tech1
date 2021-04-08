fc760793bd973bd9e1a00003c27610c85ed80032
Saiyida Noor-ul-ain Fatima <snf9718@gmail.com>
2021-04-09 00:22:28 +0500

    chapter_8

---
+++ Chapter_6/app1.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db){
+if(err) throw err;
+var dbo = db.db("mydb");
+var myobj = { name: "Company Inc", address: "Highway 37"};
+dbo.collection("customers").insertOne(myobj, function(err, res){
+    if (err) throw err;
+console.log("1 document is inserted....");
+db.close();
+});
+//dbo.createCollection("customers", function(err, res){
+//if (err) throw err;
+//console.log("Collection Created!");
+db.close();
+//});
+});
